0615cf0bde3e844a01dfa063632a870a97890954
ahmadsunnymubarok <husnimubarokslur@gmail.com>
2024-05-22 19:52:02 +0700

    button add

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>ini halaman utam</p>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="bg-white text-black">
+        <p>Soon.......................</p>
+        <Link key={"i"} href="/dashboard" className="bg-yellow-500">
+          <button>go to dashboard</button>
+        </Link>
+      </div>
     </main>
   );
 };
